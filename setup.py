aa794c67c2fd7f70ad1f15e31fe5e7404e5f6a25
Phil Marshall <dr.phil.marshall@gmail.com>
2016-11-04 11:03:29 -0700

    More detailed setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ from distutils.core import setup
 
 setup(# package information
       name="om10",
-      version="0.0.1dev",
+      version="0.7",
+      author="Phil Marshall",
+      author_email="dr.phil.marshall@gmail.com"
       description='A toolkit to use the OM10 catalog ...',
-      long_description=""" Not yet ... """,
-      # What code to include as packages
+      long_description=open("README.md").read(),
+      url="https://github.com/drphilmarshall/OM10",
       packages=['om10'],
       package_dir={'om10':'om10'},
-      # What data to include as packages
       include_package_data=True,
-      package_data={'om10': ['example_data/*']}
-      )
-
+      package_data={'om10': ['example_data/*']},
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "License :: OSI Approved :: MIT License",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Science/Research",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+      ],
+      install_requires=[numpy", "scipy", "matplotlib", "astropy", "sklearn"],
+)
